212620ae8dcf7a431b1cddfab66ec9ebda343fda
mdpatel007 <mihirdudhat123@gmail.com>
2026-01-29 23:04:29 +0530

    Fix env loading: use os.environ.get instead of decouple

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -82,11 +82,9 @@ AUTH_USER_MODEL = "accounts.Account"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "NAME": "/var/app/data/db.sqlite3",
     }
 }
-
-
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
