ada93128f9292f3b921ce8adde968ef399e1d79d
mdpatel007 <mihirdudhat123@gmail.com>
2026-01-29 22:47:23 +0530

    Run migrate & collectstatic automatically on EB deploy

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -129,8 +129,9 @@ CSRF_TRUSTED_ORIGINS = [
 
 
 # Email Configuration
-EMAIL_HOST = config("EMAIL_HOST")
-EMAIL_PORT = config("EMAIL_PORT", cast=int)
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
+EMAIL_HOST = config("EMAIL_HOST", default="")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
+EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
+
