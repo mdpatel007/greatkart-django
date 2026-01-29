af9d39ab09b361219a69818a896d13f4b70e9fb1
mdpatel007 <mihirdudhat123@gmail.com>
2026-01-29 21:50:51 +0530

    Fix DB config: use SQLite instead of RDS

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -16,11 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('securelogin/', admin.site.urls),    
-    path('store/', include('store.urls')),
-    path('', include('accounts.urls')), 
-    path('cart/', include('carts.urls')),
-    path('orders/', include('orders.urls')),
-] 
+    path("securelogin/", admin.site.urls),
+
+    path("store/", include("store.urls")),
+    path("", include("accounts.urls")),
+    path("cart/", include("carts.urls")),
+    path("orders/", include("orders.urls")),
+]
+
+# ✅ Media support in production
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
