816a77f7cbc895bb4fad4d07068d6f0fdc460fb5
mdpatel007 <mihirdudhat123@gmail.com>
2026-01-30 18:38:51 +0530

    Fix DB config: use SQLite instead of RDS

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -82,11 +82,12 @@ AUTH_USER_MODEL = "accounts.Account"
 DATABASES = {
     "default": 
         {
-        "NAME": os.getenv("RDS_DB_NAME", ""),
-        "USER": os.getenv("RDS_USERNAME", ""),
-        "PASSWORD": os.getenv("RDS_PASSWORD", ""),
-        "HOST": os.getenv("RDS_HOSTNAME", ""),
-        "PORT": os.getenv("RDS_PORT", "5432"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["RDS_DB_NAME"],
+        "USER": os.environ["RDS_USERNAME"],
+        "PASSWORD": os.environ["RDS_PASSWORD"],
+        "HOST": os.environ["RDS_HOSTNAME"],
+        "PORT": os.environ["RDS_PORT"],
         }
     }
 
